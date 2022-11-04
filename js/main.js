f3c83905f3c0df2284080256396b228e92605062
Sergio Henrique Rodrigues <86135798+sergiohrodrigues@users.noreply.github.com>
2022-11-04 16:06:05 -0300

    mudando imagem ao clicar no botão

--- js/main.js
+++ js/main.js
@@ -7,9 +7,19 @@ let arrow2 = document.querySelector('.arrow-white')
 
 ativar.addEventListener('click', () =>{
     mostrar.classList.toggle('mostrar');
-    arrow1.src = "./img/arrow-white.svg";
+
+    for (let i = 1; i <= 2; i++){
+        if(i == 1){
+            arrow1.src = "./img/arrow-gray.svg";
+        } else if (mostrar.classList.contains('mostrar')){
+            arrow1.src = "./img/arrow-white.svg";
+        }
+    }
+
+    mostrar.style.transition = 'transition: 2s'
 })
 
 desativar.addEventListener('click', () => {
     mostrar.classList.toggle('mostrar');
+    arrow1.src = "./img/arrow-gray.svg";
 })
